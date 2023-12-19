fixed lipschitz_f computation

learner_reachavoid.py
@@ -90,8 +90,8 @@ class Learner:
         self.lambda_lipschitz = 10 * 0.001
 
         # Maximum value for lipschitz coefficients (above this, incur loss)
-        self.max_lip_policy = 4
-        self.max_lip_certificate = 15
+        self.max_lip_policy = 2 #4
+        self.max_lip_certificate = 10 # 15
 
         self.global_minimum = 0.05
         self.N_expectation = 32 # 144
@@ -147,7 +147,7 @@ class Learner:
                    (self.env.lipschitz_f * (jnp.maximum(self.max_lip_policy, lip_policy) + 1) + 1)
 
             # Loss for expected decrease condition
-            exp_decrease, diff = self.loss_exp_decrease_vmap(self.args.verify_mesh_tau, Kmax, decrease_eps, V_state,
+            exp_decrease, diff = self.loss_exp_decrease_vmap(self.args.verify_mesh_tau, K, decrease_eps, V_state,
                                                           certificate_params, C_decrease, actions, noise_cond2_keys)
 
             if self.exp_decrease_max:

models/linearsystem_jax.py
@@ -47,8 +47,9 @@ class LinearEnv(gym.Env):
         ])
         self.W = np.zeros((2,2)) # np.diag([0.01, 0.005])
 
-        # Lipschitz coefficient of linear dynamical system is maximum sum of rows in A, B, and W matrix.
-        self.lipschitz_f = float(jnp.max(jnp.array([jnp.sum(self.A[i]) + self.B[i] + self.W[i] for i in range(len(self.A))])))
+        # Lipschitz coefficient of linear dynamical system is maximum sum of columns in A and B matrix.
+        self.lipschitz_f = float(np.max(np.sum(np.hstack((self.A, self.B)), axis=0)))
+            # float(jnp.max(jnp.array([jnp.sum(self.A[i]) + self.B[i] + self.W[i] for i in range(len(self.A))])))
 
         # Max step size (big Delta) under one step transition
         # TODO: Make big Delta adaptive (it may change based on the policy)