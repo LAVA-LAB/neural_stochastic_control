1fbfe8e944c211c7e3132e895744c8ae0d049ec8
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-01-10 13:54:57 +0100

    tweak

--- learner_reachavoid.py
+++ learner_reachavoid.py
@@ -92,7 +92,7 @@ class Learner:
             if expected_decrease_loss == 0:
                 loss_exp_decrease = jnp.mean(exp_decrease) + 0.01 * jnp.sum(jnp.multiply(counterx_indicator, exp_decrease)) / jnp.sum(counterx_indicator)
             elif expected_decrease_loss == 1:
-                loss_exp_decrease = jnp.mean(exp_decrease) + jnp.mean(exp_decrease2)
+                loss_exp_decrease = jnp.mean(exp_decrease) + 10 * jnp.mean(exp_decrease2)
 
             violations = (diff >= -verify_mesh_tau * K).astype(jnp.float32)
             violations = jnp.mean(violations)
