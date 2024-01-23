54acea372ff7db996efc720746716196a5ee6738
Thom Badings <tbadings@ip-145-137-153-185.wlan-int.ru.nl>
2024-01-23 15:07:17 +0100

    commit

--- verifier.py
+++ verifier.py
@@ -241,9 +241,6 @@ class Verifier:
                                             env.action_space.shape[0])
 
         Vdiff = np.zeros(len(check_expDecr_at))
-
-        assert len(Vdiff) == len(check_expDecr_at[:, -1])
-
         num_batches = np.ceil(len(check_expDecr_at) / args.verify_batch_size).astype(int)
         starts = np.arange(num_batches) * args.verify_batch_size
         ends = np.minimum(starts + args.verify_batch_size, len(check_expDecr_at))
@@ -268,7 +265,7 @@ class Verifier:
 
 
         # Compute mesh size for every relevant cell
-        tau = L1_cell_width2mesh(self.check_decrease[:, -1], env.state_dim)
+        tau = L1_cell_width2mesh(check_expDecr_at[:, -1], env.state_dim)
 
         # Negative is violation
         assert len(tau) == len(Vdiff)
