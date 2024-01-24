0bb6a5cda5ccb0ca6a31c66682e6f62419097098
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-01-24 09:11:30 +0100

    commit

--- learner_reachavoid.py
+++ learner_reachavoid.py
@@ -89,12 +89,12 @@ class Learner:
             # Loss in initial state set
             # TODO: Improve this loss function by also considering the mean?
             loss_init = jnp.maximum(0, jnp.max(V_state.apply_fn(certificate_params, x_init))
-                                    + lip_certificate * strengthen_eps * verify_mesh_tau_min_final - 1)
+                                    + lip_certificate * strengthen_eps * 0.0001 - 1)
 
             # Loss in unsafe state set
             loss_unsafe = jnp.maximum(0, 1/(1-probability_bound) -
                                       jnp.min(V_state.apply_fn(certificate_params, x_unsafe))
-                                      + lip_certificate * strengthen_eps * verify_mesh_tau_min_final)
+                                      + lip_certificate * strengthen_eps * 0.0001)
 
             K = lip_certificate * (self.env.lipschitz_f * (lip_policy + 1) + 1)
 
@@ -102,7 +102,7 @@ class Learner:
             loss_expdecr = self.loss_exp_decrease_vmap(verify_mesh_tau * K, V_state, certificate_params,
                                                        x_decrease + perturbation, actions, noise_cond2_keys)
 
-            loss_expdecr2 = self.loss_exp_decrease_vmap(strengthen_eps * verify_mesh_tau_min_final * K,
+            loss_expdecr2 = self.loss_exp_decrease_vmap(strengthen_eps * 0.0001 * K,
                                                         V_state, certificate_params, x_decrease + perturbation, actions, noise_cond2_keys)
 
             if self.expected_decrease_loss == 0: # Base loss function
