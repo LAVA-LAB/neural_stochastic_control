044fdb61b0a13d650e80bafe1cbafdbd62cd1a95
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-01-24 15:18:27 +0100

    commit

--- run.py
+++ run.py
@@ -216,6 +216,9 @@ verify.partition_noise(env, args)
 # Set training dataset (by plain grid over the state space)
 num_per_dimension_train = np.array(
     np.ceil((env.observation_space.high - env.observation_space.low) / args.train_mesh_cell_width), dtype=int)
+
+print(num_per_dimension_train)
+
 train_buffer = Buffer(dim = env.observation_space.shape[0])
 initial_train_grid = define_grid(env.observation_space.low + 0.5 * args.mesh_train_grid,
                                   env.observation_space.high - 0.5 * args.mesh_train_grid, size=num_per_dimension_train)
