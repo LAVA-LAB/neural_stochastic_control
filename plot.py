d4c07536118e3fd5d7994a42031ce3fefb9156e7
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-01-24 13:35:52 +0100

    commit

--- plot.py
+++ plot.py
@@ -18,7 +18,7 @@ def plot_traces(env, Policy_state, key, num_traces=10, len_traces=256, folder=Fa
     # Initialize traces
     for i in range(num_traces):
         # Reset environment
-        traces[0,i], _ = env.reset()
+        traces[0,i], key, _ = env.reset(key)
 
         for j in range(len_traces):
 
