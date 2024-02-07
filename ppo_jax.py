edea4582d374b43e506d161a3cfd606eb3bb8352
Thom Badings <tbadings@eduroam-55113-165.eduroam.ruhr-uni-bochum.de>
2024-02-07 13:20:32 +0100

    add 4d linear system

--- ppo_jax.py
+++ ppo_jax.py
@@ -431,6 +431,8 @@ def update_ppo_jit(
         iMax = (args.batch_size // args.minibatch_size)
         b_inds_mat = jnp.reshape(b_inds[:iMax * args.minibatch_size], (iMax, args.minibatch_size))
 
+        print(b_actions)
+
         val = (agent_state,
                b_obs,
                b_actions,
@@ -541,8 +543,6 @@ def update_ppo(
 
     # Create function that will return gradient of the specified function
     ppo_loss_grad_fn = jit(value_and_grad(ppo_loss, argnums=1, has_aux=True))
-
-    print(b_actions[mb_inds])
 
     for epoch in range(args.update_epochs):
         key, subkey = jax.random.split(key)
