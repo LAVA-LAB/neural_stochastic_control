45c64fa6906514d2124582aefaa28499a858e41c
Thom Badings <tbadings@ip-145-137-154-210.wlan-int.ru.nl>
2024-02-08 10:52:44 +0100

    add 4d linear system

--- run.py
+++ run.py
@@ -389,6 +389,8 @@ for i in range(args.cegis_iterations):
         elif np.min(suggested_mesh) >= current_mesh:
             print(f'\n- Skip refinement, because min. suggested mesh ({np.min(suggested_mesh):.5f}) is not smaller than the current max. value ({current_mesh:.5f})')
             verify_done = True
+        elif len(counterx) > 1000000:
+            print(f'\n- Skip refinement, the number of counterexamples is still too high')
         else:
             # Clip the suggested mesh at the lowest allowed value
             counterx_current_mesh = counterx[:, -1]
