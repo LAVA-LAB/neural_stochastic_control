c413fe67077d9ee83e7abbe0ff8b39c084d04577
Thom Badings <tbadings@ip-145-137-155-108.wlan-int.ru.nl>
2024-02-23 16:09:05 +0100

    Add dubins car benchmark

--- models/dubins_car.py
+++ models/dubins_car.py
@@ -97,8 +97,8 @@ class DubinsEnv(gym.Env):
 
         u = jnp.clip(u, -self.max_torque, self.max_torque)
 
-        x = state[0] + self.delta * self.V * np.cos(state[2])
-        y = state[1] + self.delta * self.V * np.sin(state[2])
+        x = state[0] + self.delta * self.V * jnp.cos(state[2])
+        y = state[1] + self.delta * self.V * jnp.sin(state[2])
         theta = state[2] + self.delta * (u + w[0])
 
         # Lower bound state
