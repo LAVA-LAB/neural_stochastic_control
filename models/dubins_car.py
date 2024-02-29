fix Dubin (angle normalization)

models/dubins_car.py
@@ -33,8 +33,8 @@ class DubinsEnv(gym.Env):
 
         self.variable_names = ['x', 'y', 'ang.vel.']
 
-        self.min_torque = np.array([-np.pi, -10], dtype=np.float32)
-        self.max_torque = np.array([np.pi, 10], dtype=np.float32)
+        self.min_torque = np.array([-0.5, -10], dtype=np.float32)
+        self.max_torque = np.array([0.5, 10], dtype=np.float32)
 
         self.delta = 0.1
 