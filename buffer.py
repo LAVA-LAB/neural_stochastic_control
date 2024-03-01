8bacb66756b6ff7a96acd14bbf420475d9c8348b
Thom Badings <tbadings@ip-145-137-155-108.wlan-int.ru.nl>
2024-03-01 18:07:16 +0100

    fix

--- buffer.py
+++ buffer.py
@@ -138,6 +138,9 @@ def meshgrid_jax(points, size):
 
 EPS = 1e-8
 def define_grid_jax(low, high, size, mode = 'linspace'):
+    print('low:', low)
+    print('high:', high)
+    print('size:', size)
     if mode == 'linspace':
         points = [np.linspace(low[i], high[i], size[i]) for i in range(len(size))]
     else:
