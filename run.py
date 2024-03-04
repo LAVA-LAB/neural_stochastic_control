cf21215a11c25cfb0379e6f0823d980956430221
Thom Badings <tbadings@ip-145-137-155-108.wlan-int.ru.nl>
2024-03-04 17:12:10 +0100

    fix

--- run.py
+++ run.py
@@ -358,7 +358,7 @@ for i in range(args.cegis_iterations):
             verify_done = True
         else:
             # Clip the suggested mesh at the lowest allowed value
-            min_allowed_mesh = args.mesh_verify_grid_init / args.max_refine_factor**i
+            min_allowed_mesh = args.mesh_verify_grid_init / args.max_refine_factor**refine_nr
             suggested_mesh = np.maximum(min_allowed_mesh, suggested_mesh)
 
             if args.local_refinement:
