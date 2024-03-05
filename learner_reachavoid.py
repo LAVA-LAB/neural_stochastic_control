57f8072d21a07aab95ea1582c3cf4cb98a94bfea
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-03-05 20:00:44 +0100

    debugging

--- learner_reachavoid.py
+++ learner_reachavoid.py
@@ -254,7 +254,7 @@ class Learner:
             loss_aux = loss_min_target + loss_min_init + loss_min_decrease + loss_min_unsafe
 
             # Define total loss
-            loss_total = (loss_unsafe + loss_unsafe_counterx +
+            loss_total = (loss_init + loss_unsafe + loss_unsafe_counterx +
                           loss_exp_decrease + loss_expdecr_counterx + loss_lipschitz + loss_aux)
             infos = {
                 '0. total': loss_total,
