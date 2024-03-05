a19fdabec5afc417042b97e4e195ac889513f97d
Thom Badings <tbadings@ip-145-137-155-108.wlan-int.ru.nl>
2024-03-05 12:56:53 +0100

    test learner improvement

--- learner_reachavoid.py
+++ learner_reachavoid.py
@@ -193,6 +193,7 @@ class Learner:
             expDecr_keys = jax.random.split(noise_key, (self.num_samples_decrease, self.N_expectation))
             V_decrease = jnp.ravel(V_state.apply_fn(certificate_params, samples_decrease))
             assert len((mesh_loss * K * (1-jnp.exp(V_decrease))).shape) == 1
+            assert len(jnp.ravel(mesh_loss * K * (1-jnp.exp(V_decrease)))) < 5000
 
             loss_expdecr = self.loss_exp_decrease_vmap(mesh_loss * K * (1-jnp.exp(V_decrease)), V_state, certificate_params,
                                                         samples_decrease, actions, expDecr_keys)
