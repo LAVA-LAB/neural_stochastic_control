dda1a25b3f21d95d3b88aa3a479e1fa6cfce7386
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-03-06 11:27:42 +0100

    debug improved training

--- learner_reachavoid.py
+++ learner_reachavoid.py
@@ -220,7 +220,7 @@ class Learner:
                                                         expDecr_keys_cx)
                 Vdiffs_cx_trim = cx_bool_decrease * jnp.ravel(Vdiffs_cx)
                 loss_exp_decrease = (jnp.sum(Vdiffs_trim, axis=0) + jnp.sum(Vdiffs_cx_trim, axis=0)) \
-                                        / (jnp.sum(samples_decrease_bool_not_target, axis=0) + jnp.sum(cx_bool_decrease, axis=0) + 1e-6) + 10 * jnp.max(Vdiffs_trim, axis=0)
+                                        / (jnp.sum(samples_decrease_bool_not_target, axis=0) + jnp.sum(cx_bool_decrease, axis=0) + 1e-6) #+ jnp.max(Vdiffs_trim, axis=0)
 
                 ### WEIGHTED LOSSES ###
 
@@ -243,7 +243,7 @@ class Learner:
             else:
                 loss_init = jnp.max(losses_init, axis=0)
                 loss_unsafe = jnp.max(losses_unsafe, axis=0)
-                loss_exp_decrease = jnp.sum(Vdiffs_trim, axis=0) / (jnp.sum(samples_decrease_bool_not_target, axis=0) + 1e-6) + 10* jnp.max(Vdiffs_trim, axis=0)
+                loss_exp_decrease = jnp.sum(Vdiffs_trim, axis=0) / (jnp.sum(samples_decrease_bool_not_target, axis=0) + 1e-6) #+ jnp.max(Vdiffs_trim, axis=0)
 
                 # Set counterexample losses to zero
                 loss_init_counterx = 0
