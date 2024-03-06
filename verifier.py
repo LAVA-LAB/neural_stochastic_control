9fb030d76038ef35aea6d6fd14149986ac2c254f
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-03-06 17:44:04 +0100

    tighten learner constraints by small constant

--- verifier.py
+++ verifier.py
@@ -426,7 +426,8 @@ class Verifier:
 
         print('- Difference between IBP and using Lipschitz coefficient directly:')
         V_mean = jit(V_state.apply_fn)(jax.lax.stop_gradient(V_state.params), self.check_init[:, :self.buffer.dim]).flatten()
-        arr = np.array(V_init_ub - (V_mean + lip_certificate * tau))
+        tau_part = cell_width2mesh(self.check_init[:, -1], env.state_dim, args.linfty)
+        arr = np.array(V_init_ub - (V_mean + lip_certificate * tau_part))
         print(arr)
         print(f'  Max: {np.max(arr)}; Min: {np.min(arr)}')
         print('  (positive means our method outperforms ibp)')
@@ -492,7 +493,8 @@ class Verifier:
         print('- Difference between IBP and using Lipschitz coefficient directly:')
         V_mean = jit(V_state.apply_fn)(jax.lax.stop_gradient(V_state.params),
                                        self.check_unsafe[:, :self.buffer.dim]).flatten()
-        arr = np.array(V_unsafe_lb - (V_mean - lip_certificate * tau))
+        tau_part = cell_width2mesh(self.check_unsafe[:, -1], env.state_dim, args.linfty)
+        arr = np.array(V_unsafe_lb - (V_mean - lip_certificate * tau_part))
         print(arr)
         print(f'  Max: {np.max(arr)}; Min: {np.min(arr)}')
         print('  (negative means our method outperforms ibp)')
