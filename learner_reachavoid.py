dca18a0c71fa26dbd69a3108b8bcff5d783f0c99
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-03-07 20:39:47 +0100

    new expected decrease loss; fix

--- learner_reachavoid.py
+++ learner_reachavoid.py
@@ -158,6 +158,7 @@ class Learner:
 
             # Small epsilon used in the initial/unsafe loss terms
             EPS = 1e-3
+            softplus_lip_min = 0.01
 
             # Compute Lipschitz coefficients.
             lip_certificate, _ = lipschitz_coeff(certificate_params, self.weighted, self.cplip, self.linfty)
@@ -197,7 +198,7 @@ class Learner:
 
             if self.improved_expdecrease_loss:
                 V_decrease_lb, _ = V_state.ibp_fn(certificate_params, samples_decrease, 0.05)
-                softplus_lip = jnp.maximum(1e-6, (1 - jnp.exp(-V_decrease_lb.flatten())))
+                softplus_lip = jnp.maximum(softplus_lip_min, (1 - jnp.exp(-V_decrease_lb.flatten())))
                 Vdiffs = jnp.maximum(0, V_expected - V_decrease_lb.flatten()
                                         + mesh_loss * K * softplus_lip)
             else:
@@ -232,7 +233,7 @@ class Learner:
 
                 if self.improved_expdecrease_loss:
                     V_decrease_lb, _ = V_state.ibp_fn(certificate_params, cx_samples, 0.05)
-                    softplus_lip_cx = jnp.maximum(1e-6, (1 - jnp.exp(-V_decrease_lb.flatten())))
+                    softplus_lip_cx = jnp.maximum(softplus_lip_min, (1 - jnp.exp(-V_decrease_lb.flatten())))
                     Vdiffs_cx = jnp.maximum(0, V_expected - V_decrease_lb.flatten()
                                          + mesh_loss * K * softplus_lip_cx)
                 else:
