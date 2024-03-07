f7c8928646de2a0f8c800926f9e921f6b265d326
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-03-07 21:12:03 +0100

    change readme

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,11 @@
+# Linear Env (@d)
+# L1, p=0.999 (should finish in around 4 minutes)
+python3 run.py --model LinearEnv --counterx_refresh_fraction 0.5 --counterx_fraction 0.25 --verify_batch_size 30000 --expdecrease_loss_type 2 --probability_bound 0.999 --expDecr_multiplier 100 --local_refinement --epochs 25 --mesh_refine_min 0.0000001 --mesh_loss 0.0001 --mesh_verify_grid_init 0.003 --mesh_verify_grid_min 0.003 --plot_intermediate --ppo_load_file 'ckpt/LinearEnv_seed=1_2024-03-06_10-48-38'
+
+##########
+### NOTE: BELOW ARE COMMANDS COMPATIBLE WITH THE OLD "MAIN" BRANCH!
+##########
+
 # Linear Env (2D)
 # L1 (Adapted for improved-training-sampling branch!)
 python3 run.py --model LinearEnv --counterx_refresh_fraction 0.5 --counterx_fraction 0.25 --verify_batch_size 30000 --expdecrease_loss_type 2 --probability_bound 0.99 --expDecr_multiplier 100 --local_refinement --epochs 25 --perturb_counterexamples --mesh_refine_min 0.00001 --mesh_loss 0.0001 --mesh_verify_grid_init 0.01 --mesh_verify_grid_min 0.01
