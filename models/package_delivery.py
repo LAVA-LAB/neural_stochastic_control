ce745995ac58e47c798e3454905f10bf474fe2de
Thom Badings <tbadings@ip-145-137-153-199.wlan-int.ru.nl>
2024-03-08 17:42:26 +0100

    change package delivery

--- models/package_delivery.py
+++ models/package_delivery.py
@@ -166,7 +166,7 @@ class PackageDelivery(gym.Env):
         # Sample noise value
         noise = self.sample_noise(subkey, size=(self.noise_dim,))
 
-        unsafe_penalty = jnp.sum(1000 * self.unsafe_space.jax_contains(state))
+        unsafe_penalty = jnp.sum(1000 * jnp.array([self.unsafe_space.jax_contains(state)]))
 
         target_mean = (self.target_space.high + self.target_space.low) / 2
         costs = -1 + (state[0]-target_mean[0]) ** 2 + (state[1] - target_mean[1]) ** 2 + unsafe_penalty
