d1937a794f8e890e1da7878874d7702790f44152
Thom Badings <tbadings@ip-145-137-153-199.wlan-int.ru.nl>
2024-03-08 15:05:42 +0100

    bring back higher weights for hard violations in verifier

--- verifier.py
+++ verifier.py
@@ -412,7 +412,7 @@ class Verifier:
         weights_expDecr = np.ones(len(Vdiff_center[violation_idxs]))  # np.maximum(0, Vdiff_mean[violation_idxs] + mesh_decrease[violation_idxs] * (Kprime + lip_certificate))
         # Normal violations get a weight of 1. Hard violations a weight that is higher.
         # hard_violation_idxs = (Vdiff_center[violation_idxs] + args.mesh_refine_min * (Kprime * softplus_lip[violation_idxs] + lip_certificate) > 0)
-        # weights_expDecr[hard_violation_idxs] *= 10
+        weights_expDecr[hard_violation_idxs] *= 10
         # print(f'- Increase the weight for {sum(hard_violation_idxs)} hard expected decrease violations')
 
         if compare_with_lip:
@@ -467,7 +467,7 @@ class Verifier:
 
         # Set weights: hard violations get a stronger weight
         weights_init = np.ones(len(x_init_vio_IBP))
-        # weights_init[V_init > 0] = hard_violation_weight
+        weights_init[V_init > 0] = hard_violation_weight
 
         out_of = self.env.init_space.contains(x_init_vio_IBP, dim=self.buffer.dim, delta=0)
         print(f'-- {x_init_vioNumHard} hard violations (out of {len(out_of)})')
@@ -525,7 +525,7 @@ class Verifier:
 
         # Set weights: hard violations get a stronger weight
         weights_unsafe = np.ones(len(x_unsafe_vio_IBP))
-        # weights_unsafe[V_unsafe < 0] = hard_violation_weight
+        weights_unsafe[V_unsafe < 0] = hard_violation_weight
 
         out_of = self.env.unsafe_space.contains(x_unsafe_vio_IBP, dim=self.buffer.dim, delta=0)
         print(f'-- {x_unsafe_vioHard} hard violations (out of {len(out_of)})')
